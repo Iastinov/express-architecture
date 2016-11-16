6f4a34650081e83f852b1f7e99f97a8ef3e33cb9
Iastinov <iastinov@gmail.com>
2016-11-16 22:56:51 +0200

    app is build till the user register form(w/o)

---
+++ index.js
@@ -0,0 +1,14 @@
+let express = require('express')
+
+var app = express()
+
+let env = process.env.NODE_ENV || 'development'
+let config = require('./server/config/config')[env]
+
+require('./server/config/database')(config)
+require('./server/config/express')(config, app)
+require('./server/config/routes')(app)
+require('./server/config/passport')()
+
+app.listen(config.port)
+console.log('MOnogo DB ready')
